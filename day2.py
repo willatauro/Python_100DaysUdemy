85c1bf636b1a1386c9ae120b040fc79de1cb0d40
Willa <willatauro@gmail.com>
2021-09-18 20:38:46 +0100

    day2_2038

--- day2.py
+++ day2.py
@@ -78,13 +78,15 @@ print(f"you have {days} days, {weeks} weeks, {months} months left")
 
 # Tip calculator project
 
-
+#https://docs.python.org/3/tutorial/floatingpoint.html
+#
 print("Welcome to the tip calculator.");
 total_bill = float(input("What was the total bill? "));
 percentage_bill = int(input("What percentage tip would you like to give? 10, 12, or 15? "));
 no_of_people = int(input("How many people to split the bill? "));
 
 pay_per_person = round((total_bill + (total_bill*(percentage_bill/100)))/no_of_people,2)
+pay_per_person ="{:.2f}".format(pay_per_person)
 print(f"Each person should pay:{pay_per_person}")
 
 
