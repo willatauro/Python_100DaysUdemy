43e9f88669742279b78f1e26ac45260552588592
Willa <willatauro@gmail.com>
2021-09-18 18:41:45 +0100

    day2_641

---
+++ day2.py
@@ -0,0 +1,11 @@
+'''
+## Data types in python
+
+Strings -- you can get individual letter using [] called as substring, it is enclosed in quotes
+Integer -- write number without quotes
+Float
+Boolean
+
+
+'''
+print(123_456_789)
