71367625c82eabc7eb467632a907461b51d32199
Willa <willatauro@gmail.com>
2021-09-21 22:25:32 +0100

    21sepday4

---
+++ day4.py
@@ -0,0 +1,4 @@
+#https://en.wikipedia.org/wiki/Mersenne_Twister
+#https://www.khanacademy.org/computing/computer-science/cryptography/crypt/v/random-vs-pseudorandom-number-generators
+#https://www.askpython.com/python-modules/python-random-module-generate-random-numbers-sequences
+
