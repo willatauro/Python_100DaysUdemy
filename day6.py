fd5bb508a311ac874876a235bbd7bbe28aa825a3
Willa <willatauro@gmail.com>
2021-09-29 22:55:49 +0100

    test2909

---
+++ day6.py
@@ -0,0 +1,3 @@
+#functions
+#https://docs.python.org/3/library/functions.html
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
