ef9da785a0e4a0b22582665f4c88ba8b8b087bcc
Willa <willatauro@gmail.com>
2021-10-07 07:52:02 +0100

    day61007

--- day6.py
+++ day6.py
@@ -1,3 +1,73 @@
 #functions
 #https://docs.python.org/3/library/functions.html
 #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+#def function():
+    #do something
+
+#Call function
+#function()
+
+
+#https://www.python.org/dev/peps/pep-0008/
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+
+#for item in list_of_items:
+    #do something to each item
+
+#while something_is_true:
+    #Do something repeatedly
+
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+
+'''
+maze problem
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+ 
+    
+while not at_goal():
+    if right_is_clear():
+       turn_right()
+       move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+ 
+
+ def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+ 
+    Problem 2
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while not wall_in_front():
+        move()
+    turn_left()
+
+while not at_goal():
+    if not front_is_clear():
+        jump()
+    elif right_is_clear():
+        turn_right()
+        move()
+    else:
+        move()
+'''
+
